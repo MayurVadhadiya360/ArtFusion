e9f7917a5889dabc3ec542846e8816838170022a
Mayur <mayurvadhadiya5@gmail.com>
2024-12-15 01:10:12 +0530

    Changed database to local sqlite3

--- ArtFusion/settings.py
+++ ArtFusion/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'ArtFusion.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default': dj_database_url.parse(env("SQL_DB_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': dj_database_url.parse(env("SQL_DB_URL"))
 }
 
 
