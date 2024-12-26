2a970f84ce953ee676695e9a2647ee32a5efebb7
Mayur <mayurvadhadiya5@gmail.com>
2024-12-26 18:38:41 +0530

    DB changed to render postgre sql instance

--- ArtFusion/settings.py
+++ ArtFusion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'ArtFusion.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    # 'default': dj_database_url.parse(env("SQL_DB_URL"))
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': dj_database_url.parse(os.environ.get("SQL_DB_URL"))
 }
 
 
--- ArtFusion/wsgi.py
+++ ArtFusion/wsgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
-# print(BASE_DIR/'staticfiles')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArtFusion.settings')
 
 application = get_wsgi_application()
-# application = WhiteNoise(application, root = BASE_DIR / 'staticfiles')
