aa4ffcf296ac8a786e61cb4b2c9173bcdd1558df
Mayur <mayurvadhadiya5@gmail.com>
2024-12-26 15:22:05 +0530

    WhiteNoise configuration for staticfile serve in production

--- ArtFusion/wsgi.py
+++ ArtFusion/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR/'staticfiles')
+# print(BASE_DIR/'staticfiles')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArtFusion.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root = BASE_DIR / 'staticfiles')
+# application = WhiteNoise(application, root = BASE_DIR / 'staticfiles')
